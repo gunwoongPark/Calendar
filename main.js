65d52ff49b9d1f045c7d61a43ed0feb61b631f46
parkGunWoong <bung1438@gmail.com>
2021-10-03 13:04:57 +0900

    모달 구조 구현

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const buildCalendar = () => {
     const tbody = document.querySelector('tbody');
+    tbody.addEventListener('click', addSch)
     // date 객체 선언
     let date = new Date();
 
@@ -9,7 +10,7 @@ const buildCalendar = () => {
     let lastDate = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate();
     let days = 1;
     // 해당 월의 1일이 무슨 요일인지 파악하기 위해 설정
-    date.setMonth(11, 1);
+    date.setMonth(date.getMonth(), 1);
     let cellCnt = date.getDay();
 
     // 공백 칸 형성
@@ -28,14 +29,13 @@ const buildCalendar = () => {
         }
 
         let cell = row.insertCell();
-
         if (cellCnt === 6) {
-            cell.innerHTML = "<font color = blue>" + days;
+            cell.style.color = "blue"
         }
         else if (cellCnt === 0) {
-            cell.innerHTML = "<font color = red>" + days;
+            cell.style.color = "red"
         }
-        else cell.innerHTML = days;
+        cell.innerHTML = days;
 
         days += 1;
         cellCnt += 1;
@@ -43,6 +43,19 @@ const buildCalendar = () => {
 
     for (let idx = cellCnt; idx < 6; ++idx) {
         row.insertCell();
+    }
+}
+
+const addSch = (e) => {
+    const dim = document.querySelector('.dim');
+    const modal = document.querySelector('.modal-container');
+
+    dim.style.visibility = 'visible'
+    modal.style.visibility = 'visible'
+
+    document.querySelector('.confirm').onclick = () => {
+        modal.style.visibility = 'hidden';
+        dim.style.visibility = 'hidden';
     }
 }
 
